0ed3bca5dfe0ca82ab505d6eaf41eb1a121cc998
Ravi Desai <ravi@ravidesai.com>
2021-09-06 17:38:27 -0400

    Start a C program to run through all the stats
    This might just be the end of doing this in Ruby.  I haven't used
    JRuby in a long time, and with the GTK glib library, I think most
    features of dynamic languages are taken care of.  Now it's about just
    doing the computation on the metal.

---
+++ run.c
@@ -0,0 +1,9 @@
+// gcc -o stats -I/usr/include/x86_64-linux-gnu -I/usr/include/glib-2.0 -I/usr/lib/x86_64-linux-gnu/glib-2.0/include run.c -lglib-2.0
+
+#include <string.h>
+#include <stdlib.h>
+#include <curl/curl.h>
+
+void main () {
+  printf("foo");
+}
