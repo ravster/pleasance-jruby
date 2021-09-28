a13eed7cf3d8e9ae34266f44be5abf1d247af3af
Ravi Desai <ravi@ravidesai.com>
2021-09-28 17:20:58 -0400

    Learn some more pandas

--- run.py
+++ run.py
@@ -2,58 +2,61 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import pdb
 import sys
+import numpy as np
+import datetime as dt
+import pandas_datareader as pdr
 
 # USAGE:
-# python3 run.py DBA.csv
+# python3 run.py DBA 2021-1-1
 #
-# The CSV file must be formatted the way you get it from Yahoo Finance, or this script
-# will break.
-# date, open, high, low, close, adj.close, volume
+# This will pull data on DBA from Yahoo Finance, from 1-jan-2021 onward.
 
-df = pd.read_csv(sys.argv[1])
+# df = pdr.get_data_yahoo(sys.argv[1], sys.argv[2])
+df = pd.read_csv("DBA.csv", parse_dates=True, index_col='Date')
+print(df, 'orig df')
 
 df["SMA50"] = df["Close"].rolling(50).mean()
+df["sma14"] = df["Close"].rolling(14).mean()
 
-for i in range(1, len(df)):
-    df.loc[i, 'tr'] = max(
-        df.loc[i, 'High'] - df.loc[i, 'Low'],
-        abs(df.loc[i, 'High'] - df.loc[i-1, 'Close']),
-        abs(df.loc[i, 'Low'] - df.loc[i-1, 'Close'])
-    )
+high_low = df['High'] - df['Low']
+high_close = np.abs(df['High'] - df['Close'].shift(1))
+low_close = np.abs(df['Low'] - df['Close'].shift(1))
+ranges = pd.concat([high_low, high_close, low_close], axis=1)
+df['tr'] = np.max(ranges, axis=1)
+
 df['atr10'] = df['tr'].rolling(10).mean()
 median_atr = df['atr10'].median()
 print(median_atr)
 
 print("Begin T1 - If we cross median ATR10, then do we make money when we go in the direction of the breakout?")
-ups = 0
-downs = 0
-up_profit = up_loss = down_profit = down_loss = 0
-up_total = down_total = 0
-for i in range(11, len(df)-5):
-    previous = df.loc[i-1]
-    row = df.loc[i]
-    target = df.loc[i+5]
-    if previous.atr10 > median_atr:
-        if previous.Close > previous.Open:
-            ups+=1
-            diff = target.Close - row.Open
-            up_total += diff
-            if diff > 0:
-                up_profit+=1
-            else:
-                up_loss+=1
+# Pandas will apply this function on every row.  It's set up to be embarassingly parallel.
+def t1(x):
+    diff = x.targetclose - x.open
+    res = [None, None]
+    if x.prevatr10 > median_atr:
+        count += 1
+        if x.prevclose - x.prevopen:
+            res = [1, diff]
         else:
-            downs+=1
-            diff = row.Open - target.Close
-            down_total += diff
-            if diff > 0:
-                down_profit+=1
-            else:
-                down_loss+=1
+            res = [-1, -diff]
+    return pd.Series({"buy/sell": res[0], "p/l": res[1]})
 
-print(ups, downs, 'foo', up_profit, up_loss, down_profit, down_loss)
-print(up_total, down_total)
-print("median close", df['Close'].median())
+# We set up a dataframe that has all the information for a calculation all in one row.
+# This is what helps the function above run on each row independently, which allows for
+# Pandas to run that code on this data in a SIMD fashion over multiple CPU cores at the
+# same time.  Independent computation rocks!
+df3 = pd.DataFrame({})
+df3['prevatr10'] = df['atr10'].shift(1)
+df3['prevopen'] = df['Open'].shift(1)
+df3['prevclose'] = df['Close'].shift(1)
+df3['open'] = df['Open']
+df3['close'] = df['Close']
+df3['targetclose'] = df['Close'].shift(-5)
+t1 = df3.apply(t1, axis=1)
+t1.to_csv("t1.csv")
+print(count, total)
+
+exit(0)
 print("DONE T1")
 
 df["hh100"] = df['High'].rolling(100).max()
